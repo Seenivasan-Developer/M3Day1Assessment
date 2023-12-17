ecf3273e093fde5dfa6ccad618185260cd7dee3c
Seenivasan-Developer <148607571+Seenivasan-Developer@users.noreply.github.com>
2023-12-17 12:16:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function foo(){
+    var fname = document.getElementById("firstname").value;
+    var mname = document.getElementById("middlename").value;
+    var lname = document.getElementById("lastname").value;
+    var email = document.getElementById("email").value;
+    console.log(`First Name ${fname}`);
+    console.log(`Middle Name ${mname}`);
+    console.log(`Last Name ${lname}`);
+    console.log(`Email ${email}`);
+    }
